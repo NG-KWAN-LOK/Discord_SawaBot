2b55581e25221560e58142be9d4db98dfee69f2c
NG-KWAN-LOK <t106590052@ntut.org.tw>
2021-01-29 19:48:14 +0800

    V3.3

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 //const { process.env.prefix, process.env.token } = require("./config.json");
+if (process.env.prefix !== "*") {
+  require("dotenv").config();
+}
 const fs = require("fs");
 const Discord = require("discord.js");
+const http = require("http");
+const express = require("express");
+const app = express();
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 client.activitys = new Discord.Collection();
 client.queue = new Map();
 
+app.get("/", (request, response) => {
+  console.log(getDateTime() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+function getDateTime() {
+  var currentdate = new Date();
+  var datetime =
+    currentdate.getFullYear() +
+    "/" +
+    (currentdate.getMonth() + 1 < 10 ? "0" : "") +
+    (currentdate.getMonth() + 1) +
+    "/" +
+    (currentdate.getDate() < 10 ? "0" : "") +
+    currentdate.getDate() +
+    " " +
+    (currentdate.getHours() < 10 ? "0" : "") +
+    currentdate.getHours() +
+    ":" +
+    (currentdate.getMinutes() < 10 ? "0" : "") +
+    currentdate.getMinutes() +
+    ":" +
+    (currentdate.getSeconds() < 10 ? "0" : "") +
+    currentdate.getSeconds();
+  return datetime;
+}
 const commandFiles = fs
   .readdirSync("./commands")
   .filter((file) => file.endsWith(".js"));
@@ -27,7 +59,7 @@ console.log(client.activitys);
 client.once("ready", async () => {
   console.log("Ready!");
   setActivity();
-  setInterval(setActivity, 1000 * 60 * 4);
+  setInterval(setActivity, 1000);
 });
 
 function setActivity() {
@@ -35,6 +67,8 @@ function setActivity() {
   const activitysName = Math.floor(Math.random() * Math.floor(3));
   const activitys = client.activitys.get(activitysName.toString());
   //const activitys = client.activitys.get("1");
+  http.get("http://sawabot.glitch.me/");
+  //http.get("http://localhost:3000");
   console.log("setActivity" + activitysName, activitys.description);
   activitys.execute(client);
 }
